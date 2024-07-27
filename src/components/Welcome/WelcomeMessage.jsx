1917227a627d19ae7f0b5a6675dbf2a94f72e367
Oliver Jones <olijones44@hotmail.co.uk>
2024-07-27 19:50:56 +0100

    bug fixes

--- src/components/Welcome/WelcomeMessage.jsx
+++ src/components/Welcome/WelcomeMessage.jsx
@@ -8,51 +8,57 @@ import { FaXTwitter } from 'react-icons/fa6';
 import { Button } from '../Buttons';
 
 function WelcomeMessage({ t }) {
+  const handleScroll = (e, scrollTo) => {
+    e.preventDefault();
+    document.getElementById(scrollTo).scrollIntoView();
+  };
+
   return (
     <section className='landing center-v'>
-      <div className=''>
-        <div className='greeting-div'>
-          <h3 className='fw-regular fs-small-sub ff-pop'>{t('greet')}</h3>
-          <h1 className='fw-bold fs-heading text-primary-800'>Oliver Jones.</h1>
-          <section className='scroll flow'>
-            <div>
-              <div className='word-scroll'>
-                <span>Writer of Code 👨🏻‍💻</span>
-                <span>Player of Guitar 🎸</span>
-                <span>Drinker of Coffee ☕</span>
-                <span>Fan of Formula 1 🏎️</span>
-                <span>Lover of Music 🎵</span>
-                <span>Speaker of Polish 🥟</span>
-              </div>
+      <div className='greeting-div'>
+        <h3 className='fw-regular fs-small-sub ff-pop'>{t('greet')}</h3>
+        <h1 className='fw-bold fs-heading text-primary-800'>Oliver Jones.</h1>
+        <section className='scroll flow'>
+          <div>
+            <div className='word-scroll'>
+              <span>Writer of Code 👨🏻‍💻</span>
+              <span>Player of Guitar 🎸</span>
+              <span>Drinker of Coffee ☕</span>
+              <span>Fan of Formula 1 🏎️</span>
+              <span>Lover of Music 🎵</span>
+              <span>Speaker of Polish 🥟</span>
             </div>
-          </section>
-          <Button body={t('connectMe')} />
-          <div className='social-nav'>
-            <a
-              href='https://github.com/ojones44'
-              target='_blank'
-              rel='noreferrer'
-              className='github'
-            >
-              <FiGithub />
-            </a>
-            <a
-              href='https://www.linkedin.com/in/oliverj1994'
-              target='_blank'
-              rel='noreferrer'
-              className='linkedin'
-            >
-              <FaLinkedinIn />
-            </a>
-            <a
-              href='https://twitter.com/oliverj1994'
-              target='_blank'
-              rel='noreferrer'
-              className='twitter'
-            >
-              <FaXTwitter />
-            </a>
           </div>
+        </section>
+        <Button
+          body={t('connectMe')}
+          onClick={(e) => handleScroll(e, 'connect')}
+        />
+        <div className='social-nav'>
+          <a
+            href='https://github.com/ojones44'
+            target='_blank'
+            rel='noreferrer'
+            className='github'
+          >
+            <FiGithub />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/oliverj1994'
+            target='_blank'
+            rel='noreferrer'
+            className='linkedin'
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            href='https://twitter.com/oliverj1994'
+            target='_blank'
+            rel='noreferrer'
+            className='twitter'
+          >
+            <FaXTwitter />
+          </a>
         </div>
       </div>
     </section>
